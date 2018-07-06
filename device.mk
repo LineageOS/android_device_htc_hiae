9a7047cb51447fa0d7e409d7bd125f93026f9021
Ethan Chen <intervigil@gmail.com>
2018-07-05 19:47:26 -0700

    hiae: Remove hwui property overrides
    * These don't exist anymore
    
    Change-Id: I68204d26d1ac141fd925bea5b116bc44b4477a0b

--- device.mk
+++ device.mk
@@ -32,8 +32,6 @@ TARGET_SCREEN_WIDTH := 1080
 
 $(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-dalvik-heap.mk)
 
-$(call inherit-product-if-exists, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
-
 # Permissions
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.audio.low_latency.xml:system/etc/permissions/android.hardware.audio.low_latency.xml \

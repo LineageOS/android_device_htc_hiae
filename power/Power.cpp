bb749b4276ea772de1cc8ad07ec8ba99e08f54c5
Ethan Chen <intervigil@gmail.com>
2019-08-17 15:10:00 -0700

    hiae: Remove display helper from power HAL
    Change-Id: I412f31094ac94c1a3e9fdba8a78f3a5a9d228ba8

--- power/Power.cpp
+++ power/Power.cpp
@@ -29,7 +29,6 @@
 
 #include "Power.h"
 #include "power-helper.h"
-#include "display-helper.h"
 
 /* RPM runs at 19.2Mhz. Divide by 19200 for msec */
 #define RPM_CLK 19200
@@ -211,13 +210,6 @@ Return<void> Power::powerHint(PowerHint_1_0 hint, int32_t data) {
             {
             ATRACE_BEGIN("low-power");
 
-            if (data) {
-              // Device in battery saver mode, enable display low power mode
-              set_display_lpm(true);
-            } else {
-              // Device exiting battery saver mode, disable display low power mode
-              set_display_lpm(false);
-            }
             ATRACE_END();
             }
             break;
